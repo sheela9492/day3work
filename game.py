d08011c8032d0bc528988220ea9f4ad7d0f33f04
sheela9492 <karpagasheela@outlook.com>
2024-09-26 12:13:41 +0100

    lets play the game

---
+++ game.py
@@ -0,0 +1,47 @@
+print('''******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/[TomekK]
+*******************************************************************************
+''')
+print("Welcome to Treasure Island")
+print("Your mission is to find the treasure")
+choice1 = input('you are at a crossroad, where do you want to go? '
+                'Type "left" or "right"\n.').lower()
+
+if choice1 == "right":
+    choice2 = input('you are come to a lake. there is a island in the middle of the lake. '
+                    'Type "wait" to wait for a boat. '
+                    'Type "swim" to swim across.\n').lower()
+    if choice2 == "wait":
+        choice3 = input('You arrive at the island unharmed.'
+                        'There is house with 3doors.'
+                        ' one red and one yellow and one blue.'
+                        'which colour do you choice.\n').lower()
+        if choice3 == "red":
+            print("Burned by fire. The Game over.")
+        elif choice3 == "blue":
+            print("Eaten by beasts. The Game over.")
+        elif choice3 == "yellow":
+            print("you Win The Game.")
+        else:
+            print("Game over.")
+    else:
+        print("Attacked by trout. Game over.")
+else:
+    print("You fell in the hole. Game over.")
